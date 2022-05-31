8bd82fbf16080eb0f282180f38474a793f9e6dc8
Zairu_ <loboser.sl@gmail.com>
2022-05-31 16:55:00 -0300

    objetos creados

--- Personajes.cs
+++ Personajes.cs
@@ -1,14 +1,15 @@
-using System;
-
 namespace RPG
 {
     public class Personaje {
         public class Caracteristicas {
-            
+            int velocidad, fuerza, nivel, armadura; //  1 - 10
+            int destreza;  //  1 - 5
         }
-
         public class Datos {
-            
+            string tipo, nombre, apodo;
+            DateTime fechaDeNacimiento;
+            int edad;     //  0 - 300
+            int salud;    //  100
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ namespace RPG
     public class Program {
         public static void Main(string[] args)
         {
-            
         }
     }
 }

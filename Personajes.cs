911fc52e3511c1c810ca3efd239e8293cec36897
Zair <loboser.sl@gmail.com>
2022-06-04 11:56:12 -0300

    Avances

--- Personajes.cs
+++ Personajes.cs
@@ -1,33 +1,58 @@
 namespace RPG
 {
+    public enum Maximos
+    {
+        VelocidadMax = 10,
+        DestrezaMax = 5,
+        FuerzaMax = 10,
+        NivelMax = 10,
+        ArmaduraMax = 10,
+        SaludMax = 100,
+        DañoMax = 50000,
+    }
     public class Personaje {
-        public class Caracteristicas {
-            int velocidad, fuerza, nivel, armadura; //  1 - 10
-            int destreza;  //  1 - 5
+        Datos datos = new Datos();
+        Caracteristicas caracteristicas = new Caracteristicas();
 
-            public Caracteristicas(int Velocidad,int Destreza, int Fuerza, int Nivel, int Armadura){
-                velocidad = Velocidad;
-                destreza = Destreza;
-                fuerza = Fuerza;
-                nivel = Nivel;
-                armadura = Armadura;
-                destreza = Destreza;
-            }
-        }
-        public class Datos {
-            string tipo, nombre, apodo;
-            DateTime fechaDeNacimiento;
-            int edad;     //  0 - 300
-            int salud;    //  100
+        public Datos Datos {set;get;}
 
-            public Datos(string Tipo, string Nombre, string Apodo, DateTime FechaDeNacimiento, int Edad, int Salud){
-                tipo = Tipo;
-                nombre = Nombre;
-                apodo = Apodo;
-                fechaDeNacimiento = FechaDeNacimiento;
-                edad = Edad;
-                salud = Salud;
-            }
+        public Caracteristicas Caracteristicas {set;get;}
+
+        public int Atacar(Personaje Defensor)
+        {
+            float PoderDeAtaque = caracteristicas.Destreza * caracteristicas.Fuerza * caracteristicas.Nivel;
+            float EfectividadDeDisparo = new Random().Next(20,50); //Le reduzco algo para que sea mas "equilibrado"
+            float ValorDeAtaque = PoderDeAtaque * EfectividadDeDisparo;
+            float PoderDeDefensa = Defensor.Caracteristicas.Armadura * Defensor.Caracteristicas.Velocidad;
+            int DañoProvocado = (int)((((ValorDeAtaque * EfectividadDeDisparo) - PoderDeDefensa) / (int)Maximos.DañoMax) * 10);
+
+            Defensor.Datos.Salud -= DañoProvocado;
+            return DañoProvocado;
         }
+    }
+    public class Datos {
+        string tipo, nombre, apodo;
+        DateTime fechaDeNacimiento;
+        int edad;     //  0 - 300
+        int salud;  //  100
+
+        public string Tipo { get => tipo; set => tipo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apodo { get => apodo; set => apodo = value; }
+        public DateTime FechaDeNacimiento { get => fechaDeNacimiento; set => fechaDeNacimiento = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public int Salud { get => salud; set => salud = value; }
+
+    }
+
+    public class Caracteristicas {
+        int velocidad, fuerza, nivel, armadura; //  1 - 10
+        int destreza;  //  1 - 5
+
+        public int Velocidad { get => velocidad; set => velocidad = value; }
+        public int Destreza { get => destreza; set => destreza = value; }
+        public int Fuerza { get => fuerza; set => fuerza = value; }
+        public int Nivel { get => nivel; set => nivel = value; }
+        public int Armadura { get => armadura; set => armadura = value; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,11 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-
 namespace RPG
 {
     public class Program {
         public static void Main(string[] args)
         {
+            var ListaDePersonajes = new List<Personaje>();
+            var Funcion = new Funcion();
+            int CantidadDePersonajes = 0;
+            
+            Console.WriteLine("Ingresar la Cantidad de Personajes que lucharan: ");
+            CantidadDePersonajes = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < CantidadDePersonajes; i++)
+            {
+                Personaje NuevoPersonaje = new Personaje();
+
+                NuevoPersonaje = Funcion.CargarDatos(NuevoPersonaje);
+                NuevoPersonaje = Funcion.CargarCaracteristicas(NuevoPersonaje);
+
+                ListaDePersonajes.Add(NuevoPersonaje);
+            }
+
+            for(int i = 0; i < CantidadDePersonajes; i++)
+            {
+                Console.WriteLine(Funcion.MostrarDatos(ListaDePersonajes[i]));
+                Console.WriteLine(Funcion.MostrarCaracteristicas(ListaDePersonajes[i]));
+            }
+
+            Console.WriteLine("\nPresiona ENTER para continuar...");
+            Console.ReadLine();
+            Console.Clear();
+
         }
     }
 }

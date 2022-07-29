d832c34cb6ad1685fa51bfd446ed74c564261695
Zair <loboser.sl@gmail.com>
2022-07-29 17:11:31 -0300

    separado de la clase lineas del archivo funciones

--- Funciones.cs
+++ Funciones.cs
@@ -610,13 +610,4 @@ namespace RPG
             }
         }
     }
-    public class Lineas
-    {
-        int ganadas;
-        string nombre,apodo;
-
-        public int Ganadas { get => ganadas; set => ganadas = value; }
-        public string Nombre { get => nombre; set => nombre = value; }
-        public string Apodo { get => apodo; set => apodo = value; }
-    }
 }
